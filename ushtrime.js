1a07503fba1caa1941fbd1d73537b1b6e5173bb0
ArtSahiti <104899505+ArtSahiti@users.noreply.github.com>
2022-05-11 12:07:30 +0200

    Add files via upload

---
+++ ushtrime.js
@@ -0,0 +1,57 @@
+//var llojiVetures = "TOYOTA";
+//var x = 50;
+var x =5;
+var y=10;
+//console.log(x+"art"+ y);
+//var z = x+y;
+
+//alert(z);
+//var s= 3; var m= "ssss"; var a=7;
+//console.log(x*y);
+
+//alert(x*y);
+
+//console.log(x/y);
+
+//alert(x/y);
+//var objekti={
+
+//name :"arti",
+//surname :"sahiti"
+
+
+
+//}
+//console.log(objekti);
+//alert(objekti.name);
+
+//var1.toUpperCase(adsdsff);
+//var2.toLowerCase(ddddddd);
+var vargu=new Array();
+
+vargu.push("arti");
+vargu.push("asass");
+vargu.push("assass");
+vargu.push("dasddas");
+console.log(vargu);
+//alert(vargu.length);
+
+vargu.pop();
+vargu.pop();
+vargu.push("jeep");
+
+var emri= "Hello World";
+var x=7
+var y=5
+
+if(x>y){
+    console.log(emri)
+
+}
+else{
+    x<y
+alert(emri)
+}
+
+
+
